f700330d045a864ea807a28e6265313349b9d79d
LJ951 <124690250+LJ951@users.noreply.github.com>
2026-01-09 00:59:15 +0800

    完善了工程

---
+++ Project/MDK-ARM/Project/lsm6dsv16x_reg.d
@@ -0,0 +1,5 @@
+project\lsm6dsv16x_reg.o: ..\HardWare\MS6DSV\lsm6dsv16x_reg.c
+project\lsm6dsv16x_reg.o: ..\HardWare\MS6DSV\lsm6dsv16x_reg.h
+project\lsm6dsv16x_reg.o: D:\APP\keil5\ARM\ARMCOMPLIER506\Bin\..\include\stdint.h
+project\lsm6dsv16x_reg.o: D:\APP\keil5\ARM\ARMCOMPLIER506\Bin\..\include\stddef.h
+project\lsm6dsv16x_reg.o: D:\APP\keil5\ARM\ARMCOMPLIER506\Bin\..\include\math.h
---
+++ Project/MDK-ARM/Project/oled_data.d
@@ -0,0 +1,3 @@
+project\oled_data.o: ..\HardWare\OLED\OLED_Data.c
+project\oled_data.o: ..\HardWare\OLED\OLED_Data.h
+project\oled_data.o: D:\APP\keil5\ARM\ARMCOMPLIER506\Bin\..\include\stdint.h
